5c036fcc46c7b4d85dd0da54de7d104ad9c430f5
JuanCamiloTest <143204710+JuanCamiloTest@users.noreply.github.com>
2023-10-08 12:40:30 -0500

    Update types-literals.java

--- types-literals.java
+++ types-literals.java
@@ -1,1 +1,23 @@
+public class HelloWorld{
 
+     public static void main(String []args){
+        System.out.println("Hello, World!\n\n");
+        
+        TypeLiteralsInJava tlj = new TypeLiteralsInJava(78L);// 78L es el literal de 
+        tlj.toStringCustom();
+     }
+}
+
+class TypeLiteralsInJava {
+    
+    int num;
+    
+    public TypeLiteralsInJava(long numP) {
+        num = (int)numP;
+    }
+    
+    public void toStringCustom() {
+        System.out.printf("variable num: %d", this.num);
+    }
+    
+}
